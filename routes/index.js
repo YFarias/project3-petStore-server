658cf372afc394966f3239e3c9e895254f628c1e
YFarias <yurisfarias@gmail.com>
2021-12-15 21:56:33 +0000

    deploy

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ const router = require("express").Router();
 // ********* require fileUploader in order to use it *********
 const fileUploader = require("../config/cloudinary.config");
 
+router.get("/", (req, res, next) => {
+  res.json("All good in here");
+});
+
+
 //Post/api/upload -Used for Uploading an image
 router.post("/api/upload", 
 fileUploader.single("image"), (req,res,next) =>{
